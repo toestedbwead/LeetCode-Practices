47a10f8fa720ba6fcd506632eb00781dce941a85
toestedbwead <leielcano@gmail.com>
2025-12-10 12:08:00 +0800

    Running Sum of 1d Array

---
+++ 1480.py
@@ -0,0 +1,14 @@
+# Running Sum of 1d Array
+# maintains a cumulative total as i iterate through an array
+# each position stores the sum of all elements up to that point
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        total = 0
+        result = []
+
+        for num in nums:
+            total+=num
+            result.append(total)
+        return result
+
